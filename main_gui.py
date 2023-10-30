0252119bce96487db982c413f5aacda59051b750
RobertPaulPro <robertpaulprophoto@gmail.com>
2023-10-29 20:53:55 -0400

    update to gui
    made window bigger and centered buttons

--- main_gui.py
+++ main_gui.py
@@ -15,16 +15,28 @@ def run_script2():
 # Create the main window
 window = tk.Tk()
 window.title("Script Launcher")
+# Set the initial size (width x height)
+window.geometry("800x600")  # Set your desired width and height
+
+# Create a frame to center the buttons
+frame = tk.Frame(window)
+frame.pack(expand=True, fill='both')
+
+# Set grid weights and row configurations for the frame
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_columnconfigure(0, weight=1)
+frame.grid_columnconfigure(1, weight=1)
 
 # Create buttons to launch the scripts
-button1 = tk.Button(window, text="DCIM Download", command=run_script1)
-button2 = tk.Button(window, text="Camera Folder Clean-up", command=run_script2)
+button1 = tk.Button(frame, text="DCIM Download", command=run_script1)
+button2 = tk.Button(frame, text="Camera Folder Clean-up", command=run_script2)
 
-# Pack the buttons in the window
-button1.pack()
-button2.pack()
+# Use grid to center the buttons in the frame
+button1.grid(row=0, column=0, padx=10, pady=10)
+button2.grid(row=0, column=1, padx=10, pady=10)
 
 # Start the Tkinter event loop
 window.mainloop()
+
 
 
